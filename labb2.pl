019947f6cdd6fcbde8259160dd84284d7b33463f
Flaxen <alexander.carlsson@live.se>
2019-10-21 10:29:10 +0200

    added functionality for verifying goals

--- labb2.pl
+++ labb2.pl
@@ -1,11 +1,63 @@
+
+% used implementations from lab 1
+
+last([Greger|[]], [], Greger).
+last([H|T], [H|R], X) :-
+  last(T, R, X).
+
+
+
+% Labb 2
+
 verify(InputFileName) :-
   see(InputFileName),
   read(Prems), read(Goal), read(Proof),
   seen,
   valid_proof(Prems, Goal, Proof).
 
+valid_proof(Prems, Goal, Proof) :-
+  checkGoal(Goal, Proof),
+  checkProof(Prems, Proof).
+
+
+checkGoal(Goal, Proof) :-
+  last(Proof, _, LastLine),
+  lineResult(LastLine, Goal), !.
+
+checkProof(Prems, Proof) :-
+  % TODO: Implement plz
+
+
+
+% Line operations
+
+lineRow([Nr, _, _], Nr).
+lineResult([_, Result, _], Result).
+lineOpertaion([_, _, Operation], Operation).
 
 
 
 
-valid_proof(Prems, Goal, Proof).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% this comment is instentional
