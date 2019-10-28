56dc3e49a4a95eecb361cef83cd430d136d2705c
joja3228 <joja3228@student.su.se>
2019-10-28 11:51:49 +0100

    ordered checkLine and added function checkBox

--- labb2.pl
+++ labb2.pl
@@ -32,7 +32,10 @@ checkProof(ValidPartProofs, [H|T]) :-
   checkProof([H|ValidPartProofs], T).
   % write(ValidPartProofs),!.
 
-
+% checks that the box is correct.
+checkBox(ValidPartProofs, [[H|T]]):-
+  checkLine(H, ValidPartProofs),
+  checkBox([H,ValidPartProofs],[[T]]).
 
 
 % Line operations
@@ -48,16 +51,16 @@ checkLine([_, P, premise], ValidPartProofs) :-
   member(P, ValidPartProofs).
 
   % assumption
-% checkLine([[Nr, P, assumption]|_], [Nr, P , assumption]).
+checkLine([[_, _, assumption]], _).
+
+  % copy
+checkLine([_, P, copy(X)], ValidPartProofs) :-
+member([X, P, _], ValidPartProofs).
 
   % andint
 checkLine([_, and(P,Q), andint(X,Y)], ValidPartProofs):-
   member([X, P, _], ValidPartProofs),
   member([Y, Q, _], ValidPartProofs).
-
-  % copy
-checkLine([_, P, copy(X)], ValidPartProofs) :-
-  member([X, P, _], ValidPartProofs).
 
   % andel1
 checkLine([_, P, andel1(X)], ValidPartProofs) :-
@@ -75,23 +78,29 @@ checkLine([_, or(P,_), orint1(X)], ValidPartProofs):-
 checkLine([_, or(_,Q), orint2(X)], ValidPartProofs):-
   member([X, Q, _], ValidPartProofs).
 
-  % negel
-checkLine([_, cont, negel(X,Y)], ValidPartProofs):-
-  member([X, P, _], ValidPartProofs),
-  member([Y, neg(P), _], ValidPartProofs).
+  % orel
 
-  % negnegint
-checkLine([_, P, negnegint(X)], ValidPartProofs):-
-  member([X, neg(neg(P)), _], ValidPartProofs).
+  % impint
 
   % impel
 checkLine([_, P, impel(X,Y)], ValidPartProofs) :-!,
   member([X, Origin, _], ValidPartProofs),!,
   member([Y, imp(Origin,P), _], ValidPartProofs),!.
 
+  % negint
+
+  % negel
+checkLine([_, cont, negel(X,Y)], ValidPartProofs):-
+  member([X, P, _], ValidPartProofs),
+  member([Y, neg(P), _], ValidPartProofs).
+
   %contel
 checkLine([_, _, contel(X)], ValidPartProofs) :-
   member([X, cont, _], ValidPartProofs).
+
+  % negnegint
+checkLine([_, P, negnegint(X)], ValidPartProofs):-
+  member([X, neg(neg(P)), _], ValidPartProofs).
 
   %negnegel
 checkLine([_, P, negnegel(X)], ValidPartProofs) :-
@@ -102,7 +111,9 @@ checkLine([_, neg(P), mt(X,Y)], ValidPartProofs) :-
   member([X, imp(P,Grej), _], ValidPartProofs),
   member([Y, neg(Grej), _], ValidPartProofs).
 
-  %lem
+  % PBC
+
+  %LEM
 checkLine([_, or(P, neg(P)), lem], _).
 
 
