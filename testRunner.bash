751b1ba107dffc7adc4e07d720d472f3d6ee5b2d
Flaxen <alexander.carlsson@live.se>
2019-10-30 12:55:24 +0100

    added post-test summary to testRunner

--- testRunner.bash
+++ testRunner.bash
@@ -1,5 +1,8 @@
+testCount=0
+passCount=0
 
 for file in tests/*.txt; do
+	testCount=$((testCount+1))
 
 	if [[ $file == *"invalid"* ]] || [[ $file == *"Invalid"* ]]; then
 
@@ -7,5 +10,15 @@ for file in tests/*.txt; do
 	else
 		variable=`prolog -q -l tests/testRunner.pl "tests/$(basename "$file")" valid`
 	fi
+
+	if [[ $variable == *"passed"* ]]; then
+		passCount=$((passCount+1))
+	fi
+
 	echo $variable
 done
+
+	echo ""
+	echo Tests executed: $testCount
+	echo Tests passed: $passCount
+	echo Tests failed: $((testCount-passCount))
